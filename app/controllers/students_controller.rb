e4a4ca6b3e042a07e07022f16d20c2f9eee760dc
Ariel Feingold <feingold.ariel@gmail.com>
2018-04-18 17:01:50 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -10,5 +10,22 @@ class StudentsController < ApplicationController
     redirect_to student_path(@student)
   end
 
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    @student.update(student_params(:first_name, :last_name))
+    redirect_to school_class_path(@student)
+  end
+
+  def student_params(*args)
+    params.require(:school_class).permit(*args)
+  end
 
 end
