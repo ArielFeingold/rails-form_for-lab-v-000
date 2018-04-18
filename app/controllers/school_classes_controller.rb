7d007ff96f947ab7f328839b307c36eb85c49ca4
Ariel Feingold <feingold.ariel@gmail.com>
2018-04-18 16:24:47 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -3,6 +3,7 @@ class SchoolClassesController < ApplicationController
   def new; end
 
   def create
+    byebug
     @school_class = SchoolClass.new(params.required(:school_class).permit(:title, :room_number))
     @school_class.save
     redirect_to school_class_path(@school_class)
